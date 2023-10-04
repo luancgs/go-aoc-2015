a72d82cf8e247c98d7c29a38da114b8042c60204
Luan <luancgs@proton.me>
2023-10-04 12:37:54 -0300

    feat: adds day 03 solution

---
+++ day03/day03.go
@@ -0,0 +1,85 @@
+package day03
+
+import (
+	"fmt"
+
+	input "github.com/luancgs/go-aoc-2015/utils"
+)
+
+const day = 3
+
+func formatInput(input string) []rune {
+	return []rune(input)
+}
+
+func Part1(reader input.InputReader) int {
+	directions := formatInput(reader.GetInput(day))
+
+	set := make(map[string]bool)
+	set["0,0"] = true
+
+	x := 0
+	y := 0
+
+	for _, direction := range directions {
+		switch direction {
+		case '^':
+			y++
+		case '>':
+			x++
+		case 'v':
+			y--
+		case '<':
+			x--
+		}
+
+		set[fmt.Sprintf("%d,%d", x, y)] = true
+	}
+
+	return len(set)
+}
+
+func Part2(reader input.InputReader) int {
+	directions := formatInput(reader.GetInput(day))
+
+	set := make(map[string]bool)
+	set["0,0"] = true
+
+	santaX := 0
+	santaY := 0
+	robotX := 0
+	robotY := 0
+
+	isSanta := true
+
+	for _, direction := range directions {
+		if isSanta {
+			switch direction {
+			case '^':
+				santaY++
+			case '>':
+				santaX++
+			case 'v':
+				santaY--
+			case '<':
+				santaX--
+			}
+			set[fmt.Sprintf("%d,%d", santaX, santaY)] = true
+		} else {
+			switch direction {
+			case '^':
+				robotY++
+			case '>':
+				robotX++
+			case 'v':
+				robotY--
+			case '<':
+				robotX--
+			}
+			set[fmt.Sprintf("%d,%d", robotX, robotY)] = true
+		}
+		isSanta = !isSanta
+	}
+
+	return len(set)
+}
--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/luancgs/go-aoc-2015/day02"
+	"github.com/luancgs/go-aoc-2015/day03"
 	input "github.com/luancgs/go-aoc-2015/utils"
 )
 
 func main() {
 
-	output1 := day02.Part1(input.Reader{})
+	output1 := day03.Part1(input.Reader{})
 
-	fmt.Println("Day 02 - Part 1 | Output: ", output1)
+	fmt.Println("Day 03 - Part 1 | Output: ", output1)
 
-	output2 := day02.Part2(input.Reader{})
+	output2 := day03.Part2(input.Reader{})
 
-	fmt.Println("Day 02 - Part 2 | Output: ", output2)
-
+	fmt.Println("Day 03 - Part 2 | Output: ", output2)
 }
