04be5fe74aa9708cd37f0837e1ebd8e50d607579
Luan <luancgs@proton.me>
2023-10-04 12:52:02 -0300

    feat: adds day 04 solution

---
+++ day04/day04.go
@@ -0,0 +1,36 @@
+package day04
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+
+	input "github.com/luancgs/go-aoc-2015/utils"
+)
+
+const day = 4
+
+func hash(key string, prefix string) int {
+	hash := ""
+	i := 1
+
+	for !strings.HasPrefix(hash, prefix) {
+		data := fmt.Sprintf("%s%d", key, i)
+		hash = fmt.Sprintf("%x", md5.Sum([]byte(data)))
+		i++
+	}
+
+	return i - 1
+}
+
+func Part1(reader input.InputReader) int {
+	secretKey := reader.GetInput(day)
+	output := hash(secretKey, "00000")
+	return output
+}
+
+func Part2(reader input.InputReader) int {
+	secretKey := reader.GetInput(day)
+	output := hash(secretKey, "000000")
+	return output
+}
--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/luancgs/go-aoc-2015/day03"
+	"github.com/luancgs/go-aoc-2015/day04"
 	input "github.com/luancgs/go-aoc-2015/utils"
 )
 
 func main() {
 
-	output1 := day03.Part1(input.Reader{})
+	output1 := day04.Part1(input.Reader{})
 
-	fmt.Println("Day 03 - Part 1 | Output: ", output1)
+	fmt.Println("Day 04 - Part 1 | Output: ", output1)
 
-	output2 := day03.Part2(input.Reader{})
+	output2 := day04.Part2(input.Reader{})
 
-	fmt.Println("Day 03 - Part 2 | Output: ", output2)
+	fmt.Println("Day 04 - Part 2 | Output: ", output2)
 }
