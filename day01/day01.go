0bd9e7741b91314cde97bcd58fd52ea5effd3943
Luan <luancgs@proton.me>
2023-10-04 09:07:12 -0300

    feat: adds day01 solution

---
+++ day01/day01.go
@@ -0,0 +1,41 @@
+package day01
+
+import input "github.com/luancgs/go-aoc-2015/utils"
+
+func formatInput(input string) []rune {
+	return []rune(input)
+}
+
+func Part1(reader input.InputReader) int {
+	content := reader.GetInput(1)
+
+	var floor int = 0
+	for _, char := range formatInput(content) {
+		if char == '(' {
+			floor++
+		} else if char == ')' {
+			floor--
+		}
+	}
+
+	return floor
+}
+
+func Part2(reader input.InputReader) int {
+	content := reader.GetInput(1)
+
+	var floor int = 0
+	for index, char := range formatInput(content) {
+		if char == '(' {
+			floor++
+		} else if char == ')' {
+			floor--
+		}
+
+		if floor == -1 {
+			return index + 1
+		}
+	}
+
+	return 0
+}
