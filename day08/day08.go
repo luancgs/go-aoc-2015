7170985b79ee99857a98d8a8603f09060c4839a0
Luan <luancgs@proton.me>
2023-10-05 18:47:07 -0300

    feat: adds day 08 solution

---
+++ day08/day08.go
@@ -0,0 +1,63 @@
+package day08
+
+import (
+	"regexp"
+	"strings"
+
+	input "github.com/luancgs/go-aoc-2015/utils"
+)
+
+const day = 8
+
+func formatInput(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+func Part1(reader input.InputReader) int {
+	lines := formatInput(reader.GetInput(day))
+
+	codeTotal := 0
+	literalTotal := 0
+
+	for _, line := range lines {
+
+		if line == "" {
+			continue
+		}
+		codeTotal += len(line)
+
+		line := strings.TrimPrefix(line, "\"")
+		line = strings.TrimSuffix(line, "\"")
+		line = strings.ReplaceAll(line, "\\\\", "-")
+		line = strings.ReplaceAll(line, "\\\"", "-")
+		m := regexp.MustCompile(`(\\x[0-9a-fA-F]{2})`)
+		line = m.ReplaceAllString(line, "-")
+
+		literalTotal += len(line)
+	}
+
+	return codeTotal - literalTotal
+}
+
+func Part2(reader input.InputReader) int {
+	lines := formatInput(reader.GetInput(day))
+
+	codeTotal := 0
+	literalTotal := 0
+
+	for _, line := range lines {
+
+		if line == "" {
+			continue
+		}
+
+		codeTotal += len(line)
+
+		line = strings.ReplaceAll(line, "\\", "\\\\")
+		line := strings.ReplaceAll(line, "\"", "\\\"")
+
+		literalTotal += len(line) + 2
+	}
+
+	return literalTotal - codeTotal
+}
